ca15f18dbe96ebab9ebb5f82149c99f6c87271a8
Cole Santana <csantan3@asu.edu>
2019-02-20 20:00:26 -0700

    Initial version of Calculator.java

---
+++ HOMEWORK2/src/cse360assign2/Calculator.java
@@ -0,0 +1,72 @@
+package cse360assign2;
+/**
+ * @author      Cole Santana 
+ * @classNumber 541              
+ * @date        2-19-19         
+ */
+public class Calculator {
+		
+		private int total;
+	/**
+	 * Calculator - constructor for the class
+	 * 
+	 * @param total - set the total to 0
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal - returns the final total of the calculation
+	 * 
+	 * @return - result of all the operations
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add - adds the inputed values
+	 * 
+	 * @param value - integer applied to the function
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract - subtracts the inputed values
+	 * 
+	 * @param value - integer applied to the function
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply - multiplies the inputed values
+	 * 
+	 * @param value - integer applied to the function
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide - divides the inputed values
+	 * 	
+	 * @param value - integer applied to the function
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory - prints out the respected functions and the end result
+	 * 
+	 * @return - String of the operations.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
