b6c91f0fb9f413e4c83fd40b70b776323df83f89
coles <coles@LAPTOP-7M8GQIVK>
2019-02-23 13:08:46 -0700

    2nd version with methods implemented

--- HOMEWORK2/src/cse360assign2/Calculator.java
+++ HOMEWORK2/src/cse360assign2/Calculator.java
@@ -22,7 +22,7 @@ public class Calculator {
 	 * @return - result of all the operations
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
@@ -31,7 +31,7 @@ public class Calculator {
 	 * @param value - integer applied to the function
 	 */
 	public void add (int value) {
-		
+		total += value;
 	}
 	
 	/**
@@ -40,7 +40,7 @@ public class Calculator {
 	 * @param value - integer applied to the function
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
 	}
 	
 	/**
@@ -49,7 +49,7 @@ public class Calculator {
 	 * @param value - integer applied to the function
 	 */
 	public void multiply (int value) {
-		
+		total *= value;
 	}
 	
 	/**
@@ -58,7 +58,9 @@ public class Calculator {
 	 * @param value - integer applied to the function
 	 */
 	public void divide (int value) {
-		
+		if (value == 0)
+			total = 0;
+		total /= value;
 	}
 	
 	/**
